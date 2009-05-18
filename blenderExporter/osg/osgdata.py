5679e3fbb8b29e1178b90b6697d2c60fe33f52d4
Cedric Pinson <mornifle@plopbyte.net>
2009-05-18 14:56:35 +0200

    Alberto: fix bug while exporting avatar blender file\nAdded a way to export data correctly if texture use an uv layer not present in mesh\nUpdate debug message with warning\nUpdated test\n

--- blenderExporter/osg/osgdata.py
+++ blenderExporter/osg/osgdata.py
@@ -282,7 +282,7 @@ def getBakedIpos(obj, ori_ipo, anim_fps):
 
     return new_ipo
     for frame in range(staframe, endframe+1):
-        debug(80,'Baking Frame %i' % frame)
+        if DEBUG: debug(80,'Baking Frame %i' % frame)
 		#tell Blender to advace to frame
         Blender.Set(CURFRAME,frame) # computes the constrained location of the 'real' objects
         if not BATCH: Blender.Redraw() # no secrets, let user see what we are doing
@@ -296,7 +296,7 @@ def getBakedIpos(obj, ori_ipo, anim_fps):
         
         time = Blender.Get('curtime') #for BezTriple
         ipos = addPoint(time,key,ipos) #add this data at this time to the ipos
-        debug(100,'%s %i %.3f %.2f %.2f %.2f %.2f %.2f %.2f' % (myipoName, myframe, time, key[0], key[1], key[2], key[3], key[4], key[5]))
+        if DEBUG: debug(100,'%s %i %.3f %.2f %.2f %.2f %.2f %.2f %.2f' % (myipoName, myframe, time, key[0], key[1], key[2], key[3], key[4], key[5]))
     
 
     new_ipo = animtion_bake_constraints.bakeFrames(obj, new_ipo)
@@ -692,7 +692,7 @@ class Export(object):
             # TODO: I'm not sure what's going on here? :)
             # It means it's an solid object animation.
             if obj_name == 'Object':
-                log("Warning dont support Object Action export (%s)" % action_name)
+                log("WARNING dont support Object Action export (%s)" % action_name)
                 return None
 
             self.convertIpoToAnimation(obj_name, animation, ipo)
@@ -771,7 +771,7 @@ class Export(object):
             st = StateSet()
             self.root.stateset = st
             if len(self.lights) > 8:
-                log("warning more than 8 lights")
+                log("WARNING more than 8 lights")
 
             # retrieve world to global ambient
             lm = LightModel()
@@ -894,9 +894,10 @@ class BlenderObjectToGeometry(object):
     def createTexture2D(self, mtex):
         image_object = mtex.tex.getImage()
         if image_object is None:
-            log("Warning the texture % has not Image, skip it" % mtex.tex.getName())
+            log("WARNING the texture %s has not Image, skip it" % mtex.tex.getName())
             return None
         texture = Texture2D()
+        texture.name = mtex.tex.getName()
         filename = "//" + Blender.sys.basename(image_object.getFilename().replace(" ","_"))
         texture.file = filename.replace("//","textures/")
         texture.source_image = image_object
@@ -905,6 +906,7 @@ class BlenderObjectToGeometry(object):
     def createStateSet(self, index_material, mesh, geom):
         s = StateSet()
         uvs = geom.uvs
+        if DEBUG: debug("geometry uvs %s" % (str(uvs)))
         geom.uvs = {}
         if len(mesh.materials) > 0:
             # support only one material by mesh right now
@@ -917,7 +919,7 @@ class BlenderObjectToGeometry(object):
                 m.diffuse = (mat_source.R * refl, mat_source.G * refl, mat_source.B * refl, mat_source.alpha)
 
                 # if alpha not 1 then we set the blending mode on
-                debug("state material alpha %s" % str(mat_source.alpha))
+                if DEBUG: debug("state material alpha %s" % str(mat_source.alpha))
                 if mat_source.alpha != 1.0:
                     s.modes.append(("GL_BLEND", "ON"))
 
@@ -934,7 +936,7 @@ class BlenderObjectToGeometry(object):
                 s.attributes.append(m)
 
                 texture_list = mat_source.getTextures()
-                debug("texture list %s" % str(texture_list))
+                if DEBUG: debug("texture list %s" % str(texture_list))
 
                 # find a default channel if exist uv
                 default_uv = None
@@ -946,18 +948,20 @@ class BlenderObjectToGeometry(object):
                 for i in range(0, len(texture_list)):
                     if texture_list[i] is not None:
                         t = self.createTexture2D(texture_list[i])
-                        debug("texture %s %s" % (i, texture_list[i]))
+                        if DEBUG: debug("texture %s %s" % (i, texture_list[i]))
                         if t is not None:
                             if not s.texture_attributes.has_key(i):
                                 s.texture_attributes[i] = []
                             uv_layer =  texture_list[i].uvlayer
-                            if len(uv_layer) > 0:
-                                debug("texture %s use uv layer %s" % (i, uv_layer))
+                            if len(uv_layer) > 0 and not uvs.has_key(uv_layer):
+                                log("WARNING your material '%s' with texture '%s' use an uv layer '%s' that does not exist on the mesh '%s', use the first uv channel as fallback" % (mat_source.getName(), t.name, uv_layer, geom.name))
+                            if len(uv_layer) > 0 and uvs.has_key(uv_layer):
+                                if DEBUG: debug("texture %s use uv layer %s" % (i, uv_layer))
                                 geom.uvs[i] = TexCoordArray()
                                 geom.uvs[i].array = uvs[uv_layer].array
                                 geom.uvs[i].index = i
                             elif default_uv:
-                                debug("texture %s use default uv layer %s" % (i, default_uv_key))
+                                if DEBUG: debug("texture %s use default uv layer %s" % (i, default_uv_key))
                                 geom.uvs[i] = TexCoordArray()
                                 geom.uvs[i].index = i
                                 geom.uvs[i].array = default_uv.array
@@ -968,12 +972,11 @@ class BlenderObjectToGeometry(object):
                                     s.modes.append(("GL_BLEND","ON"))
                             except:
                                 log("can't read the source image file for texture %s" % t)
-                debug("state set %s" % str(s))
+                if DEBUG: debug("state set %s" % str(s))
 
         # adjust uvs channels if no textures assigned
-        print "result uvs ", geom.uvs.keys()
         if len(geom.uvs.keys()) == 0:
-            debug("no texture set, adjust uvs channels, in arbitrary order")
+            if DEBUG: debug("no texture set, adjust uvs channels, in arbitrary order")
             index = 0
             for k in uvs.keys():
                 uvs[k].index = index
@@ -1026,14 +1029,14 @@ class BlenderObjectToGeometry(object):
             if face.mat != material_index:
                 continue
             f = []
-            fdebug = []
+            if DEBUG: fdebug = []
             for vertex in face.verts:
                 index = len(vertexes)
                 vertexes.append(vertex)
                 f.append(index)
-                fdebug.append(vertex.index)
-            debug("true face %s" % str(fdebug))
-            debug("face %s" % str(f))
+                if DEBUG: fdebug.append(vertex.index)
+            if DEBUG: debug("true face %s" % str(fdebug))
+            if DEBUG: debug("face %s" % str(f))
             collected_faces.append((face,f))
 
         if (len(collected_faces) == 0):
@@ -1094,21 +1097,19 @@ class BlenderObjectToGeometry(object):
             index = len(mapping_vertexes)
             merged_vertexes[i] = index
             mapping_vertexes.append([i])
-            if DEBUG is True:
-                debug("process vertex %s" % i)
+            if DEBUG: debug("process vertex %s" % i)
             for j in range(i+1, len(vertexes)):
                 if tagged_vertexes[j] is True: # avoid processing more than one time a vertex
                     continue
                 different = self.equalVertices(i, j, vertexes, normals, colors, uvs)
                 if not different:
-                    if DEBUG is True:
-                        debug("   vertex %s is the same" % j)
+                    if DEBUG: debug("   vertex %s is the same" % j)
                     merged_vertexes[j] = index
                     tagged_vertexes[j] = True
                     mapping_vertexes[index].append(j)
 
 
-        if DEBUG is True:
+        if DEBUG:
             for i in range(0, len(mapping_vertexes)):
                 debug("vertex %s contains %s" % (str(i), str(mapping_vertexes[i])))
 
@@ -1120,15 +1121,13 @@ class BlenderObjectToGeometry(object):
         faces = []
         for (original, face) in collected_faces:
             f = []
-            fdebug = []
+            if DEBUG: fdebug = []
             for v in face:
                 f.append(merged_vertexes[v])
-                if DEBUG is True:
-                    fdebug.append(vertexes[mapping_vertexes[merged_vertexes[v]][0]].index)
+                if DEBUG: fdebug.append(vertexes[mapping_vertexes[merged_vertexes[v]][0]].index)
             faces.append(f)
-            if DEBUG is True:
-                debug("new face %s" % str(f))
-                debug("true face %s" % str(fdebug))
+            if DEBUG: debug("new face %s" % str(f))
+            if DEBUG: debug("true face %s" % str(fdebug))
             
         log("faces %s" % str(len(faces)))
 
@@ -1145,7 +1144,7 @@ class BlenderObjectToGeometry(object):
             verts = {}
             for idx, weight in mesh.getVertsFromGroup(i, 1):
                 if weight < 0.001:
-                    log( "warning " + str(idx) + " to has a weight too small (" + str(weight) + "), skipping vertex")
+                    log( "WARNING " + str(idx) + " to has a weight too small (" + str(weight) + "), skipping vertex")
                     continue
                 if original_vertexes2optimized.has_key(idx):
                     for v in original_vertexes2optimized[idx]:
@@ -1153,7 +1152,7 @@ class BlenderObjectToGeometry(object):
                             verts[v] = weight
                         #verts.append([v, weight])
             if len(verts) == 0:
-                log( "warning " + str(i) + " has not vertexes, skip it, if really unsued you should clean it")
+                log( "WARNING " + str(i) + " has not vertexes, skip it, if really unsued you should clean it")
             else:
                 vertex_weight_list = [ list(e) for e in verts.items() ]
                 vg = VertexGroup()
@@ -1183,7 +1182,7 @@ class BlenderObjectToGeometry(object):
                 osg_uvs[name].array.append(uvs[name][vindex])
 
         if (len(osg_uvs)):
-            log("uvs channels %s" % len(osg_uvs))
+            log("uvs channels %s - %s" % (len(osg_uvs), str(osg_uvs.keys())))
 
         nlin = 0
         ntri = 0
@@ -1198,7 +1197,7 @@ class BlenderObjectToGeometry(object):
             elif nv == 4:
                 nquad = nquad + 1
             else:
-                log("Warning can't manage faces with %s vertices" % nv)
+                log("WARNING can't manage faces with %s vertices" % nv)
 
         # counting number of primitives (one for lines, one for triangles and one for quads)
         numprims = 0
@@ -1278,7 +1277,7 @@ class BlenderObjectToGeometry(object):
 
     def convert(self):
         if self.mesh.vertexUV:
-            log("Warning mesh %s use sticky UV and it's not supported" % self.object.getName())
+            log("WARNING mesh %s use sticky UV and it's not supported" % self.object.getName())
 
         list = self.process(self.mesh)
         return list
