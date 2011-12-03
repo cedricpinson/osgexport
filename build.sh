1317177419cb8f1a7f5dd6d601f75508c00f55e6
Cedric Pinson <cedric.pinson@plopbyte.com>
2011-12-03 01:22:35 +0100

    Update README instructions

---
+++ build.sh
@@ -0,0 +1,5 @@
+cd blender-2.5/exporter/
+version=$(cat osg/__init__.py | grep 'VERSION' | sed 's/VERSION.*"\(.*\)"/\1/')
+appname=osgexport-$version
+echo $appname
+zip -r ../build/${appname}.zip .
