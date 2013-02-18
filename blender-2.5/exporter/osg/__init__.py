e4ba1922c1307c4f9d108ce6fc4f9076de62c263
Cedric Pinson <cedric.pinson@plopbyte.com>
2013-02-18 20:49:19 +0100

    Add meta for materials

--- blender-2.5/exporter/osg/__init__.py
+++ blender-2.5/exporter/osg/__init__.py
@@ -25,7 +25,7 @@ import pickle
 bl_info = {
     "name": "Export OSG format (.osgt)",
     "author": "Cedric Pinson, Jeremy Moles, Peter Amstutz",
-    "version": (0,13,0),
+    "version": (0,14,0),
     "blender": (2, 6, 3),
     "email": "jeremy@emperorlinux.com, cedric.pinson@plopbyte.com, peter.amstutz@tseboston.com",
     "api": 36339,
