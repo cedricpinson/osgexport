01a2c12d2a3348018471a0a4c52ded7e89ff09bd
Cedric Pinson <cedric@plopbyte.com>
2014-06-18 11:58:36 +0200

    Update exporter version

--- blender-2.5/exporter/osg/__init__.py
+++ blender-2.5/exporter/osg/__init__.py
@@ -25,7 +25,7 @@ import pickle
 bl_info = {
     "name": "Export OSG format (.osgt)",
     "author": "Cedric Pinson, Jeremy Moles, Peter Amstutz",
-    "version": (0,14,1),
+    "version": (0,14,2),
     "blender": (2, 6, 3),
     "email": "trigrou@gmail.com, jeremy@emperorlinux.com, peter.amstutz@tseboston.com",
     "api": 36339,
